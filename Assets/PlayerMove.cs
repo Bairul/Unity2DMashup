38f306e0fb256b60c019a5b7dda29124a54754ca
Bairu <bairuli1234@gmail.com>
2024-09-09 12:00:21 -0700

    movement

--- Assets/PlayerMove.cs
+++ Assets/PlayerMove.cs
@@ -2,17 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
+
 public class PlayerMove : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Rigidbody2D rgbd2d;
+    public float mvtSpd;
+    private Vector2 mvt;
+    
 
     // Update is called once per frame
     void Update()
     {
+        mvt.x = Input.GetAxisRaw("Horizontal");
+        mvt.y = Input.GetAxisRaw("Vertical");
+        mvt.Normalize();
         
+        rgbd2d.velocity = mvt * mvtSpd;
     }
 }
